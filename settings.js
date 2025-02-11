e650a5e3df0060daf440cd18ac06ff7e3f111d56
vfxtv-boop <bbaralangui@gmail.com>
2025-02-11 23:54:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "242067410576"
+global.pairingNumber = "242041029122"
 
 
 
